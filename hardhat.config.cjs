d68a9658a4ddb30243a235f5b19e5893eb7e6dad
buildinginweb3 <tchanstudies@gmail.com>
2026-01-05 01:48:01 +0800

    Add files via upload

---
+++ hardhat.config.cjs
@@ -0,0 +1,20 @@
+// hardhat.config.cjs
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config = {
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    baseSepolia: {
+      url: "https://sepolia.base.org",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  }
+};
+
+module.exports = config;
